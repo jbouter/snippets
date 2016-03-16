ba774f5d2909ccc0b18b66ea4335fefe9f3ac2b7
kyentei <kyentei@kunix.org>
2016-03-16 13:07:17 +0100

    +setwallpaper.sh

---
+++ i3/setwallpaper.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Check whether it's after 6 in the evening or not
+if [ $(date +%H) -ge 18 ]; then
+  # It's nighttime, set the purple buck wallpaper
+  feh --bg-fill ~/documents/owncloud/wallpapers/buck-day.png
+else
+  # It's daytime, set the blue buck wallpaper
+  feh --bg-fill ~/documents/owncloud/wallpapers/buck-night.png
+fi

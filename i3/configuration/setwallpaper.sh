7c83cd0e284762393fec687654ec0f1d4bc7d71a
kyentei <kyentei@kunix.org>
2016-03-24 14:59:36 +0100

    didn't want the trees wallpapers, have something awesome now :)

--- i3/configuration/setwallpaper.sh
+++ i3/configuration/setwallpaper.sh
@@ -6,16 +6,7 @@ else
   SETDISPLAY=":0.0"
 fi
 
-# Between 8 PM and 4 AM, I want the 'night' wallpaper
-if [ $(date +%H) -ge 20 ] && [ $(date +%H) -lt 4 ]; then
-  DAYTIME=night
-# Between 4 AM and 12 PM, I want the 'morning' wallpaper
-elif [ $(date +%H) -ge 4 ] && [ $(date +%H) -lt 12 ]; then
-  DAYTIME=morning
-# Between 12 PM and 8 PM, I want the 'afternoon' wallpaper
-elif [ $(date +%H) -ge 12 ] && [ $(date +%H) -lt 20 ]; then
-  DAYTIME=afternoon
-fi
+HOUR=$(date +%H)
+WALL=$(($HOUR/2*2))
 
-# Set the actual wallpaper
-DISPLAY=$SETDISPLAY feh --bg-fill ~/documents/owncloud/wallpapers/tree_$DAYTIME.jpg
+DISPLAY=$SETDISPLAY feh --bg-fill ~/documents/owncloud/wallpapers/time_based/$WALL.png

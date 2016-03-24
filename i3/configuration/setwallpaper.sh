13ce1ecd1e8957d1fc14a475730b21597c03235b
kyentei <kyentei@kunix.org>
2016-03-24 14:28:35 +0100

    fixed an and/or oopsie

--- i3/configuration/setwallpaper.sh
+++ i3/configuration/setwallpaper.sh
@@ -7,13 +7,13 @@ else
 fi
 
 # Between 8 PM and 4 AM, I want the 'night' wallpaper
-if [ $(date +%H) -ge 20 ] || [ $(date +%H) -lt 4 ]; then
+if [ $(date +%H) -ge 20 ] && [ $(date +%H) -lt 4 ]; then
   DAYTIME=night
 # Between 4 AM and 12 PM, I want the 'morning' wallpaper
-elif [ $(date +%H) -ge 4 ] || [ $(date +%H) -lt 12 ]; then
+elif [ $(date +%H) -ge 4 ] && [ $(date +%H) -lt 12 ]; then
   DAYTIME=morning
 # Between 12 PM and 8 PM, I want the 'afternoon' wallpaper
-elif [ $(date +%H) -ge 12 ] || [ $(date +%H) -lt 20 ]; then
+elif [ $(date +%H) -ge 12 ] && [ $(date +%H) -lt 20 ]; then
   DAYTIME=afternoon
 fi
 

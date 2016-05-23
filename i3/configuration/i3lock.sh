7f77990e91f9d9558345a527164668d3e85cccac
kyentei <kyentei@kunix.org>
2016-05-23 15:47:18 +0200

    updated lockscreen

--- i3/configuration/i3lock.sh
+++ i3/configuration/i3lock.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 #scrot /tmp/screen_locked.png
 #convert /tmp/screen_locked.png -blur 8x8 -colorspace gray /tmp/screen_locked2.png
-i3lock -i ~/documents/owncloud/wallpapers/green_owl.png
+i3lock -i ~/documents/owncloud/wallpapers/staring_owl_resized.png

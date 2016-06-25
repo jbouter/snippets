feb81a403448c7ff1223569cfa9e3c02171a0a5b
kyentei <kyentei@kunix.org>
2016-06-25 14:48:00 +0200

    updated setwallpaper to use /r/wallpapers

--- i3/configuration/setwallpaper.sh
+++ i3/configuration/setwallpaper.sh
@@ -17,7 +17,7 @@ fi
 
 # Set the wallpaper
 #DISPLAY=$SETDISPLAY feh --bg-fill ~/documents/owncloud/wallpapers/deer.jpg
-DISPLAY=$SETDISPLAY wallpaper-reddit
+DISPLAY=$SETDISPLAY wallpaper-reddit --random --force
 
 # Set screen blanking to 1hr
 DISPLAY=$SETDISPLAY xset s 3600 3600 &

f060ceab77ab8731af548524342469c44786d779
kyentei <kyentei@kunix.org>
2016-07-07 16:52:04 +0200

    updated wallpaper

--- i3/configuration/setwallpaper.sh
+++ i3/configuration/setwallpaper.sh
@@ -17,7 +17,7 @@ fi
 
 # Set the wallpaper
 #DISPLAY=$SETDISPLAY wallpaper-reddit --random --force
-DISPLAY=$SETDISPLAY feh --bg-fill "/home/jeffrey/documents/owncloud/wallpapers/elemental_stripes.jpg"
+DISPLAY=$SETDISPLAY feh --bg-fill "/home/jeffrey/documents/owncloud/wallpapers/square_galaxy.jpg"
 
 # Set screen blanking to 1hr
 DISPLAY=$SETDISPLAY xset s 3600 3600 &

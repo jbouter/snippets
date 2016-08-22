c135f68e0042748d210815df8115b3f957f4fed3
kyentei <kyentei@kunix.org>
2016-08-22 23:34:32 +0200

    updated wallpaper

--- i3/configuration/setwallpaper.sh
+++ i3/configuration/setwallpaper.sh
@@ -17,7 +17,7 @@ fi
 
 # Set the wallpaper
 #DISPLAY=$SETDISPLAY wallpaper-reddit --random --force
-DISPLAY=$SETDISPLAY feh --bg-fill "/home/jeffrey/documents/owncloud/wallpapers/adwaita_dark.jpg"
+DISPLAY=$SETDISPLAY feh --bg-fill "/home/jeffrey/documents/owncloud/wallpapers/hexagons.png"
 
 # Set screen blanking to 1hr
 DISPLAY=$SETDISPLAY xset s 3600 3600 &

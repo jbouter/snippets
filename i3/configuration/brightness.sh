9e998fb0e35d21403c688f3e1d2e12836bf0afab
kyentei <kyentei@kunix.org>
2016-08-23 21:53:39 +0200

    made brightness Nvidia compatible, set config to only add or substract 1 from this value

--- i3/configuration/brightness.sh
+++ i3/configuration/brightness.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+#
+# Default block, don't remove this (setting variables)
+#
+
 # base dir for backlight class
 basedir="/sys/class/backlight/"
 
@@ -12,15 +16,42 @@ old_brightness=$(cat $handler"brightness")
 # get max brightness
 max_brightness=$(cat $handler"max_brightness")
 
+#
+# End default block
+#
+
+
+#
+# Below block works with Nvidia drivers
+#
+
+# Add or substract $1
+new_brightness=$(( $old_brightness $1))
+
+# Don't go above the max brightness, don't go below 1
+if [ $old_brightness -lt 15 ] && [ $old_brightness -gt 1 ]; then
+  # set the new brightness value
+  sudo chmod 666 $handler"brightness"
+  echo $new_brightness > $handler"brightness"
+fi
+
+#
+# End Nvidia driver
+#
+
+#
+# Below block works with Nouveau drivers
+#
+
 # get current brightness %
-old_brightness_p=$(( 100 * $old_brightness / $max_brightness ))
+#old_brightness_p=$(( 100 * $old_brightness / $max_brightness ))
 
 # calculate new brightness % 
-new_brightness_p=$(($old_brightness_p $1))
+#new_brightness_p=$(($old_brightness_p $1))
 
 # calculate new brightness value
-new_brightness=$(( $max_brightness * $new_brightness_p / 100 ))
+#new_brightness=$(( $max_brightness * $new_brightness_p / 100 ))
 
-# set the new brightness value
-sudo chmod 666 $handler"brightness"
-echo $new_brightness > $handler"brightness"
+#
+# End Nouveau drivers
+#
